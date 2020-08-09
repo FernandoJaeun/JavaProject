3f9e17ef96235c5e6f21f77ca86c647b30a87f7a
leeja <Leejaeyun95@naver.com>
2020-08-09 23:31:55 +0900

    constructor정의 공부

---
+++ java_basic/class_instance/constructor/AppleStore.java
@@ -0,0 +1,15 @@
+package java_basic.class_instance.constructor;
+
+public class AppleStore {
+    public static void main(String[] args) {
+        FruitByer jaeyun = new FruitByer();
+        FruitSeller seoyun = new FruitSeller();
+
+        int cash = 5700;
+        jaeyun.buyApple(seoyun, cash);
+        jaeyun.showBuyResult(jaeyun.getClass().getSimpleName());
+
+        seoyun.showSaleResult(seoyun.getClass().getSimpleName());
+    }
+    
+}
---
+++ java_basic/class_instance/constructor/FruitSeller.java
@@ -0,0 +1,24 @@
+package java_basic.class_instance.constructor;
+
+public class FruitSeller {
+    private static final int APPLE_PRICE = 1000;
+    private static int apples = 20;
+    private static int wallet = 0;
+
+    
+    public int[] saleApple(int income) {
+        final int[] arr = new int[2];
+
+        arr[0] = income / APPLE_PRICE; // 사과
+        arr[1] = income % APPLE_PRICE; // 거스름돈 
+        apples -= arr[0];
+        wallet += arr[0] * APPLE_PRICE;
+        return arr;
+    }
+
+    public void showSaleResult(String name){
+        System.out.println("--" + name + "--");
+        System.out.println("남은 사과 : " + apples);
+        System.out.println("판매 수익 : " + wallet);
+    }
+}
